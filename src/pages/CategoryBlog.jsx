c997401950c181ab691dfb14e462ba5988c98cee
MD.MARUF SHEIKH <marufsheikh803@gmail.com>
2025-10-28 18:24:40 +0600

    data fetch

---
+++ src/pages/CategoryBlog.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CategoryBlog = () => {
+    return (
+        <div>
+            <h1>this is category blogs</h1>
+        </div>
+    );
+};
+
+export default CategoryBlog;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Blogs from './Blogs';
+import Category from './Category';
+import CategoryBlog from './CategoryBlog';
 
 const Home = () => {
+    const [blogs, setBlogs]= useState([]);
+    const [categories, setCategories] = useState([]);
+    // Blogs data
+    useEffect(()=>{
+        fetch('blogs.json')
+        .then(res => res.json())
+        .then(data => setBlogs(data))
+    },[])
+
+    // Category Data
+    useEffect(()=>{
+        fetch('category.json')
+        .then(res => res.json())
+        .then(data => setCategories(data)
+        )
+    },[])
     return (
         <div>
-            <h1>this is home page</h1>
+            <div className='container px-7 mx-auto grid grid-cols-12 gap-4 mt-4'>
+                <div className='col-span-8 border border-gray-400 p-3'>
+                    <Blogs></Blogs>
+                </div>
+                <div className='col-span-4border border-gray-400 p-3'>
+                    <Category></Category>
+                    <CategoryBlog></CategoryBlog>
+                </div>
+            </div>
         </div>
     );
 };
